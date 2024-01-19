3b242f27b5b6ba27bf1d9df372e57fffe8eb3e4f
ehgur062300 <ehgur062300@gmail.com>
2024-01-19 17:30:00 +0900

    프로젝트 초기 설정 및 MySQL 연동

---
+++ src/test/java/com/example/codeleader/CodeLeaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeleader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLeaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

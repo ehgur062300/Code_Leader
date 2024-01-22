5cbddeb16ac9115a27d57ffdaaf65338998343fa
ehgur062300 <ehgur062300@gmail.com>
2024-01-22 20:29:07 +0900

    domain : Member 수정

--- src/main/java/com/example/codeleader/domain/member/Member.java
+++ src/main/java/com/example/codeleader/domain/member/Member.java
@@ -1,6 +1,8 @@
 package com.example.codeleader.domain.member;
 
 import com.example.codeleader.domain.BaseTimeEntity;
+import com.example.codeleader.domain.posts.Post;
+import com.example.codeleader.domain.tech.Tech;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,20 +13,28 @@ import java.util.List;
 
 @Entity
 @NoArgsConstructor
-@Getter @Setter
+@Getter
+@Setter
 public class Member extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="member_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
 
     private String memberName;
+
+    private String password;
+
+    private String nickName;
+
+    private String email;
 
     private Role role;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<Tech> techList = new ArrayList<>();
 
-
-
+    @OneToMany(mappedBy = "writer", cascade = CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
 }

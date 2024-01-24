75a900034740ddb418a6857e9f727bf5ed859747
ehgur062300 <ehgur062300@gmail.com>
2024-01-24 11:36:44 +0900

    domain : Post 및 Tech 연관 테이블 생성

--- src/main/java/com/example/codeleader/domain/tech/PostTech.java
+++ src/main/java/com/example/codeleader/domain/tech/PostTech.java
@@ -1,4 +1,27 @@
 package com.example.codeleader.domain.tech;
 
-public class PostTech {
+import com.example.codeleader.domain.BaseTimeEntity;
+import com.example.codeleader.domain.posts.Post;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+public class PostTech extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_tech_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tech_id")
+    private Tech tech;
+
 }
